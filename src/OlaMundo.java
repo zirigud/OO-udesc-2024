fd6ba06b9c5891c8663578e6753260dd99908475
Arthur Perret <thuiperret1@gmail.com>
2024-08-19 21:48:58 -0300

    TESTE

---
+++ src/OlaMundo.java
@@ -0,0 +1,13 @@
+public class OlaMundo {
+
+        String nome = "Udesc";
+        int Idade = 41;
+        void ImprimirDados () {
+            System.out.println(nome + " - " + Idade);
+        }
+
+        void Imprimir (String nome,int Idade){
+            System.out.println(nome + " - " + Idade);
+        }
+    }
+
---
+++ src/TesteOlaMundod.java
@@ -0,0 +1,8 @@
+public class TesteOlaMundod {
+    public static void main(String[] args) {
+        OlaMundo ola = new OlaMundo();
+        ola.ImprimirDados();
+        ola.Imprimir("Fulano",18 );
+
+    }
+}
